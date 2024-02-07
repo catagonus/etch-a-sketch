9ded9d4edc22a4586c079f338942c3bbdefc4c24
catagonus <107582977+catagonus@users.noreply.github.com>
2024-02-07 13:33:04 -0500

    Rework entire app - 3 different ink colors, random color, eraser, and clear work

--- javascript.js
+++ javascript.js
@@ -2,23 +2,12 @@ const grid = document.querySelector('#grid');
 
 const GRIDSIZE = 640;
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('span');
-    row.style.display = 'flex';
-    row.style.border = 'none';
-    for (let j = 0; j < 16; j++) {
-        const cell = document.createElement('div');
-        row.appendChild(cell);
-        
-    }
-    grid.appendChild(row);
-}
+let drawMode = 'blackink';
+let drawToggle = 'on';
 
-makeDraw();
+drawGrid(16);
 
-function drawGrid() {
-
-    const size = prompt('Enter a grid size 1-100.');
+function drawGrid(size) {
     
     while (grid.firstChild) {
         grid.removeChild(grid.lastChild);
@@ -30,63 +19,116 @@ function drawGrid() {
         row.style.border = 'none';
         for (let j = 0; j < size; j++) {
             const cell = document.createElement('div');
-            cell.style.border = '1px solid black';
+            cell.classList.add('drawdiv');
             cell.style.height = GRIDSIZE / size + 'px';
             cell.style.width = GRIDSIZE / size + 'px';
             row.appendChild(cell);
         }
-        
         grid.appendChild(row);
     }
-
-    makeDraw();
-}
-
-function makeDraw() {
-
-    const drawDivs = document.querySelectorAll('#grid div');
-
+    
+    const drawDivs = document.querySelectorAll('.drawdiv');
     drawDivs.forEach(div => {
-        div.setAttribute('class', 'drawOn');
-        div.addEventListener('mouseenter', draw);
-        div.addEventListener('click', toggleDraw);
-    });
-
-    function draw(e) {
-        if (e.target.classList.contains('drawOn')) {
-            e.target.style.backgroundColor = 'chartreuse';
-        }
-    }
-    
-    function clear() {
-        drawDivs.forEach(div => {
-            div.addEventListener('mouseenter', draw);
-            div.setAttribute('class', 'drawOn');
-            div.style.backgroundColor = '';
-        })
-    }
-    
-    function toggleDraw(e) {
-        if (e.target.classList.contains('drawOn')) {
-            drawDivs.forEach(div => {
-                div.classList.remove('drawOn');
-                div.setAttribute('class', 'drawOff');
-            });
-        } else {
-            drawDivs.forEach(div => {
-                div.classList.remove('drawOff');
-                div.setAttribute('class', 'drawOn');
-            });
-        }
-        draw(e);
-    }
-    
-    const clearButton = document.querySelector('#clear');
-    
-    clearButton.addEventListener('click', clear);
-    
-    const userSizeButton = document.querySelector('#userSize');
-    
-    userSize.addEventListener('click', drawGrid);
+        div.addEventListener('mouseover', draw);
+        div.addEventListener('mousedown', toggleDraw);
+        div.addEventListener('mouseup', draw);
+    })
 }
+
+function draw(e) {
+    if (drawToggle === 'on') {
+        switch(drawMode) {
+            case 'blackink':
+                e.target.style.backgroundColor = 'black';
+                break;
+            case 'redink':
+                e.target.style.backgroundColor = 'red';
+                break;
+            case 'greenink':                
+                e.target.style.backgroundColor = 'chartreuse';
+                break;
+            case 'blueink':                
+                e.target.style.backgroundColor = 'blue';
+                break;
+            case 'randomink':
+                randomColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+                e.target.style.backgroundColor = randomColor;
+                break;
+            case 'eraser':                
+                e.target.style.backgroundColor = '';
+                break;
+        }
+    }
+}
+
+const blackColor = document.querySelector('#black');
+
+blackColor.addEventListener('click', () => {
+    currentColor = 'black';  
+    drawMode = 'blackink';
+});
+
+const redColor = document.querySelector('#red');
+
+redColor.addEventListener('click', () => {
+    drawMode = 'redink';
+});
+
+const greenColor = document.querySelector('#green');
+
+greenColor.addEventListener('click', () => {
+    drawMode = 'greenink';
+});
+
+const blueColor = document.querySelector('#blue');
+
+blueColor.addEventListener('click', () => {
+    drawMode = 'blueink';
+});
+
+const randomizer = document.querySelector('#random');
+
+randomizer.addEventListener('click', () => {
+    drawMode = 'randomink';
+});
+
+const eraseButton = document.querySelector('#eraser');
+
+eraseButton.addEventListener('click', () => {
+    drawMode = 'eraser';
+});
+
+const userSizeButton = document.querySelector('#userSize');
+
+function makeUserGrid() {
+    let userInputSize = prompt('Enter a grid size 1-100.');
+    drawGrid(userInputSize);
+}
+
+userSizeButton.addEventListener('click', makeUserGrid);
+
+const clearButton = document.querySelector('#clear');
+    
+clearButton.addEventListener('click', clear);
+
+function clear() {
+    const drawDivs = document.querySelectorAll('.drawdiv');
+    drawDivs.forEach(div => {
+        div.style.backgroundColor = '';
+    })
+}
+
+function toggleDraw(e) {
+    if (drawToggle === 'on') {
+        drawToggle = 'off';
+    } else if (drawToggle === 'off') {
+        drawToggle = 'on';
+        
+    };
+}
+    
+
+
+
+
 
